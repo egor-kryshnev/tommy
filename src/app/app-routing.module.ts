a0b97d699b4102cf56e61f55e1dc41eaedd91d69
ronabet <ronabet456@gmail.com>
2020-02-26 16:52:11 +0200

    all components to create request"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { TasksComponent } from './tasks/tasks.component';
 import { UpdatingComponent } from './updating/updating.component';
 import { HomeComponent } from './home/home.component';
 import { NetworksComponent } from './networks/networks.component';
-
+import { ServicesComponent } from './services/services.component';
+import { DescriptionComponent } from './description/description.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'newtask', component: NetworksComponent }
+  { path: 'newtask', component: NetworksComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'description', component: DescriptionComponent },
 ];
 
 @NgModule({
