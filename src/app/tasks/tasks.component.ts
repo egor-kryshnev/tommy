c99cfe26d829addf7bb8b5d41177ec9d533d7f67
vitaly-furman <furman4488@gmail.com>
2020-02-27 10:49:38 +0200

    added main page app-tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { NewTaskComponent } from '../new-task/new-task.component';
+
 
 export interface Pnia {
   title: string;
@@ -16,58 +19,98 @@ export interface Pnia {
 })
 export class TasksComponent implements OnInit {
 
-  
+
+  selectedOpenTasks: Boolean = true;
 
   tasks: Pnia[] = [
-    { title: "Send File",
+    {
+      title: "Send File",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "Change Computer",
+      time: new Date()
+    },
+    {
+      title: "Change Computer",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "3",
+      time: new Date()
+    },
+    {
+      title: "Mouse not working",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "4",
+      time: new Date()
+    },
+    {
+      title: "Speaker not working",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "5",
+      time: new Date()
+    },
+    {
+      title: "רמקולים לא עובדים",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "6",
+      time: new Date()
+    },
+    {
+      title: "Speaker not working",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "7",
+      time: new Date()
+    },
+    {
+      title: "Speaker not working",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "8",
+      time: new Date()
+    },
+    {
+      title: "Speaker not working",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "9",
+      time: new Date()
+    },
+    {
+      title: "Speaker not working",
       date: new Date(),
       id: 20035489612,
-      time: new Date() },
-    { title: "10",
+      time: new Date()
+    },
+    {
+      title: "Speaker not working",
       date: new Date(),
       id: 20035489612,
-      time: new Date() }
+      time: new Date()
+    }
   ]
 
- 
 
-  constructor() { }
+
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
-  
+  onOpenDialog() {
+    const dialogRef = this.dialog.open(NewTaskComponent, {
+      width: '1280px',
+      height: '720px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+  clickedOpenTasks() {
+    if (!this.selectedOpenTasks) this.selectedOpenTasks = true;
+  }
+
+  clickedClosedTasks() {
+    if (this.selectedOpenTasks) this.selectedOpenTasks = false;
+  }
+
+
 
 }
