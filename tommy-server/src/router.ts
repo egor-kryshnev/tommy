390358520bb1f37e3265bbc4a450cb3af3e68552
egor-kryshnev <egor.kryshnev@gmail.com>
2020-03-01 13:13:07 +0200

    feat: add path /api/user to return the shraga user to the client

--- tommy-server/src/router.ts
+++ tommy-server/src/router.ts
@@ -3,5 +3,6 @@ import { Router, Request, Response } from "express";
 const AppRouter: Router = Router();
 
 AppRouter.get('/isalive', (req: Request, res: Response) => { res.status(200).send('Server Is Up'); });
+AppRouter.get('/user', (req: Request, res: Response) => { res.status(200).json(req.user); });
 
 export { AppRouter };
