a7d157077bcdcc372193c9ae79241af80227d1d7
yoavrozner <rozner.yoav@gmail.com>
2020-03-17 20:09:48 +0200

    fix: the opened tasks are now displayed and get from the api the full stats for every task for the task-details

--- tommy-client/src/app/post-req.service.ts
+++ tommy-client/src/app/post-req.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { config } from 'src/environments/config.dev';
 
-
 export interface PostResponse {
   "cr": {
     "@COMMON_NAME": string
--- tommy-client/src/app/task-detail/task-detail.component.ts
+++ tommy-client/src/app/task-detail/task-detail.component.ts
@@ -14,4 +14,17 @@ export class TaskDetailDialog {
   getDesc(): string {
     return this.data.description.length <= 200 ? this.data.description : this.data.description.substring(0, 200) + '...';
   }
+
+  getOpenDate(): string {
+    const dateObj = new Date(this.data.open_date);
+    let dd = dateObj.getDate() < 10 ? '0' + dateObj.getDate() : dateObj.getDate();
+    let mm = dateObj.getMonth() + 1 < 10 ? '0' + dateObj.getMonth() + 1 : dateObj.getMonth() + 1;
+    const yyyy = dateObj.getFullYear();
+
+    return `${dd}/${mm}/${yyyy}`;
+  }
+
+  getCategory(): string {
+    return this.data.category.split('.')[0];
+  }
 }
