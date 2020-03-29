45401414ce454ce93fd1696a4c0f5ce8e4f150fa
yoavrozner <rozner.yoav@gmail.com>
2020-03-29 17:16:45 +0300

    feat: update the style and button navigation

--- tommy-client/src/app/open-request/transverse-incident/transverse-incident.component.ts
+++ tommy-client/src/app/open-request/transverse-incident/transverse-incident.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { TransverseIncident } from '../category/category.service';
 import { Router } from '@angular/router';
 
@@ -10,13 +10,15 @@ import { Router } from '@angular/router';
 })
 export class TransverseIncidentDialog {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: TransverseIncident, private router: Router) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: TransverseIncident, private router: Router, public dialogRef: MatDialogRef<TransverseIncidentDialog>) { }
 
   onHomeClick() {
     this.router.navigateByUrl('');
+    this.dialogRef.close();
   }
 
   onOpenNewIncident() {
-    this.router.navigateByUrl('/networks');
+    this.router.navigateByUrl('/newtask');
+    this.dialogRef.close();
   }
 }
