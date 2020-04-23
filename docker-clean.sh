46cf816ec6f58d2b47d86523e9715e0fff7e7655
adarospascalos <adarpascal100@gmail.com>
2020-04-23 18:56:49 +0300

    docker clean sh added

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 docker kill $(docker ps -q)
 sleep 0.2
-docker rm $(docker ps -a -q -f)
+docker rm $(docker ps -a -q)
 sleep 0.2
 docker rmi $(docker images -q) -f
+sleep 0.5
+docker ps -a
+sleep 1
+docker images
