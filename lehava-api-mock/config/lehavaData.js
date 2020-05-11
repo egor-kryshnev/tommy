272d423725320fffecc7fd63084443be30f0aaca
adarospascalos <adarpascal100@gmail.com>
2020-05-11 20:45:23 +0300

    Fix: now the mock is returning accessK Exp in Sec

--- lehava-api-mock/config/lehavaData.js
+++ lehava-api-mock/config/lehavaData.js
@@ -5,7 +5,7 @@ module.exports = {
             "@REL_ATTR": "400051",
             "@COMMON_NAME": "1835377188",
             "access_key": 59975677,
-            "expiration_date": new Date() + 100000
+            "expiration_date": new Date() / 1000 + 604800
         }
     },
 
