c70ade5c56e426d83de91a52e5d1568bd50136fa
adarospascalos <adarpascal100@gmail.com>
2020-06-21 14:50:37 +0300

    Feat: Added Networks to Updates Component

--- tommy-client/src/app/updating/updating.component.ts
+++ tommy-client/src/app/updating/updating.component.ts
@@ -19,7 +19,10 @@ export class UpdatingComponent implements OnInit {
   constructor(public apiget: ApigetService) { }
 
   ngOnInit() {
+    this.getUpdates();
+  }
 
+  getUpdates() {
     this.apiget.getUpdates().subscribe((res: any) => {
       let updatesArrayResponse = res.collection_cr.cr;
       if (updatesArrayResponse) {
@@ -39,34 +42,5 @@ export class UpdatingComponent implements OnInit {
       }
     });
   }
-
-  // this.apiget.getUpdates().subscribe((res: any) => {
-  //   this.updatesArrayRes = res.collection_cr.cr;
-  //   this.updatesObjectRes = res.collection_cr.cr;
-  //   this.array = Array.isArray(this.updatesArrayRes) ? true : false;
-  //   if (this.array) {
-  //     this.updatesArrayRes.forEach((element: any) => {
-  //       let current_datetime = new Date(element.open_date * 1000);
-  //       let formatted_date = current_datetime.getHours() + ":" + current_datetime.getMinutes() + "\xa0\xa0·\xa0\xa0" + current_datetime.getDate() + "." + (current_datetime.getMonth() + 1) + "." + current_datetime.getFullYear();
-  //       this.updatesArrayFiltered.push(
-  //         {
-  //           "name": element.category["@COMMON_NAME"].replace(/\./g, ' '),
-  //           "description": element.summary,
-  //           "open_date": formatted_date,
-  //           "z_network": element.z_network["@COMMON_NAME"] ? element.z_network["@COMMON_NAME"] : "",
-  //         }
-  //       )
-  //     });
-  //     this.updatesArrayFiltered = this.updatesArrayFiltered.reverse();
-  //   }
-  //   else {
-  //     this.updatesObject = {
-  //       "name": this.updatesObjectRes.category["@COMMON_NAME"].replace(/\./g, ' '),
-  //       "description": this.updatesObjectRes.summary,
-  //       "open_date": this.updatesObjectRes.open_date,
-  //       "z_network": this.updatesObjectRes.z_network["@COMMON_NAME"]
-  //     }
-  //   }
-  // });
 
 }
