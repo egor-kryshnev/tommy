5b7379d9706776efc4a893f4789ac57f2a160f9b
vitaly-furman <furman4488@gmail.com>
2020-06-24 10:57:01 +0300

    added chat

--- tommy-server/src/hichat.router.ts
+++ tommy-server/src/hichat.router.ts
@@ -9,20 +9,31 @@ const HichatRouter: Router = Router();
 HichatRouter.post('*', AuthorizationMiddleware.postAuthorization);
 
 // Simple Url Response for client-side development
-HichatRouter.get('/', (req: Request, res: Response) => {
+HichatRouter.get('/', async (req: Request, res: Response) => {
+    // const chat = new Chat();
+    // const user: any = req.user;
+    // // const userT: string = user.adfsId.split("@")[0];
+    // const userT: string = "t123456";
+    // const hitchatUserT: string = `${userT}@aman`;
+    // const groupName: string = chat.setGroupName(userT);
+
+    // //TODO: get support users from redis
+    // let groupUsersToAdd: string[] = (config.chat.supportUsers).slice(0);
+    // groupUsersToAdd.push(hitchatUserT);
+
+    // try {
+    //     await chat.createGroup(groupName, groupUsersToAdd.concat([hitchatUserT]));
+    // } catch (err) {
+    //     console.log(err);
+    // } finally {
+    //     await chat.setRoomMembers(groupName, groupUsersToAdd);
+    //     res.json("url", `${config.chat.hiChatUrl}/${groupName}`)
+    //     // TODO: pull support users from lehava and update in redis
+    // }
+
     res.send({ url: 'https://www.ynet.co.il/Ext/App/TalkBack/CdaViewOpenTalkBack/0,11382,L-3190779-3,00.html' });
 });
 
-// LehavaRouter.get('/hichat', async (req: Request, res: Response) => {
-// try {
-//     let chat = new Chat();
-//     let headers = await chat.getAuthHeaders();
-//     console.log(headers);
-// } catch (err) {
-//     console.log(err);
-// }
-// res.send('ITS OKAY')
-// });
 
 HichatRouter.get('/isalive', (req: Request, res: Response) => res.status(200).send('Server Is Up'));
 
---
+++ tommy-server/src/utils/supportUsers.ts
@@ -0,0 +1,2 @@
+let supportUsers = ["t1010101@aman", "t11223344@aman", "t12312300@aman"];
+export { supportUsers };
