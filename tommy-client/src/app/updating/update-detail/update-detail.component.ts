1690205efb127ed3026306731fa38927934a83a7
adarospascalos <adarpascal100@gmail.com>
2020-06-28 18:28:34 +0300

    Feat: Added updates detail sheet

---
+++ tommy-client/src/app/updating/update-detail/update-detail.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { MatBottomSheetRef, MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+
+@Component({
+  selector: 'update-detail',
+  templateUrl: './update-detail.component.html',
+  styleUrls: ['./update-detail.component.css']
+})
+export class BottomUpdateDetailSheet implements OnInit {
+  constructor(private _bottomSheetRef: MatBottomSheetRef<BottomUpdateDetailSheet>, @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) { }
+
+  ngOnInit(): void {
+    console.log(this.data)
+  }
+
+  openLink(event: MouseEvent): void {
+    this._bottomSheetRef.dismiss();
+    event.preventDefault();
+  }
+}
