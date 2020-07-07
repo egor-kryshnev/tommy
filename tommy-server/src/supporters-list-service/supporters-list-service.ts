ead9764013313132781848cc14f772b360bae64c
adarospascalos <adarpascal100@gmail.com>
2020-07-08 00:06:49 +0300

    need to convert uuid to t user

--- tommy-server/src/supporters-list-service/supporters-list-service.ts
+++ tommy-server/src/supporters-list-service/supporters-list-service.ts
@@ -9,12 +9,14 @@ export class SupportersListService {
     private static client: redis.RedisClient = redis.createClient(config.redis.host);
     private static getRedis = promisify(SupportersListService.client.get).bind(SupportersListService.client);
     private static setRedis = promisify(SupportersListService.client.set).bind(SupportersListService.client);
-    private static supportersList: Supporter[];
+    private static supportersList: String[];
 
-    public static async getSupportersList(accessToken: number): Promise<Supporter[]> {
-        if (!SupportersListService.supportersList || !await this.isValidExpirationDate()) {
+    public static async getSupportersList(accessToken: number): Promise<String[]> {
+
+        if (!SupportersListService.supportersList || ! await this.isValidExpirationDate()) {
+
             const redisSupportersList = (await this.getRedis('Supporters-List')).split(',');
-            
+
             if (redisSupportersList && this.isValidExpirationDate) {
                 console.log(`Supporters list brought from redis: ${redisSupportersList}`)
                 this.supportersList = redisSupportersList;
@@ -31,10 +33,10 @@ export class SupportersListService {
                 const supportersListRes: Supporter[] = res.data.collection_cnt.cnt;
                 console.log(`Using access key: ${accessToken}`);
                 console.log(`Setting REDIS[Supporters-List]: ${supportersListRes}`);
-                await this.setRedis('Supporters-List', supportersListRes.toString());
+                await this.setRedis('Supporters-List', this.arrayBuildByKey(supportersListRes, "@COMMON_NAME"));
                 console.log(`Setting REDIS[Supporters-Exp-Date]: ${new Date().getTime() + 86400000}`);
                 await this.setRedis('Supporters-Exp-Date', `${new Date().getTime() + 86400000}`);
-                SupportersListService.supportersList = supportersListRes;
+                SupportersListService.supportersList = this.arrayBuildByKey(supportersListRes, "@COMMON_NAME");
             }
         }
         return SupportersListService.supportersList;
@@ -44,5 +46,11 @@ export class SupportersListService {
         const currentTime = new Date().getTime();
         const expDate = parseInt(await this.getRedis('Supporters-Exp-Date'));
         return currentTime < expDate;
+    }
+
+    static arrayBuildByKey(array: any[], key: string) {
+        return array.map((elem) => {
+            return elem[key];
+        });
     }
 }
