1dd7996a5b82e94d189dd4c259c5aaf7a513be7f
CarmelTsadok <carmelcarmel88888@gmail.com>
2020-11-09 12:43:33 +0200

    feat(config): Move form data body and url to lehava-api config file

--- tommy-server/src/lehava.router.ts
+++ tommy-server/src/lehava.router.ts
@@ -13,7 +13,7 @@ LehavaRouter.post("*", AuthorizationMiddleware.postAuthorization, IpMiddleware);
 LehavaRouter.post("/file/*", async (req: Request, res: Response) => {
   const url = `http://${config.lehava_api.host}:${
     config.lehava_api.port
-  }${getRequestWithFileUrl(req.url, req.body.file)}`;
+  }${config.lehava_api.getRequestWithFileUrl(req.url, req.body.file)}`;
   try {
     const apiHeaders = { ...req.headers };
     apiHeaders["X-AccessKey"] = await AccessTokenProvider.getAccessToken();
@@ -27,7 +27,7 @@ LehavaRouter.post("/file/*", async (req: Request, res: Response) => {
         url,
         params: req.params,
         headers: apiHeaders,
-        data: getFormDataBody(
+        data: config.lehava_api.getFormDataBody(
           req.body.postType,
           req.body[req.body.postType],
           req.body.file
@@ -40,7 +40,7 @@ LehavaRouter.post("/file/*", async (req: Request, res: Response) => {
       url,
       params: req.params,
       headers: apiHeaders,
-      data: getFormDataBody(
+      data: config.lehava_api.getFormDataBody(
         req.body.postType,
         req.body[req.body.postType],
         req.body.file
@@ -84,64 +84,5 @@ LehavaRouter.all("*", async (req: Request, res: Response) => {
     res.send(e.message);
   }
 });
-
-const getRequestWithFileUrl = (
-  reqUrl: string,
-  fileObject: {
-    name: string;
-    type: string;
-    base64: string;
-  }
-): string => {
-  return `${reqUrl.split("/file")[1]}?repositoryId=1002&serverName=${
-    config.lehava_api.host
-  }&mimeType=${fileObject.type}&description=${fileObject.name}`;
-};
-
-const getFormDataBody = (
-  postType: string,
-  postObject: any,
-  file: {
-    name: string;
-    type: string;
-    base64: string;
-  }
-): string => {
-  return `--*****MessageBoundary*****\r
-  Content-Disposition: form-data; name="${postType}"
-  Content-Type: application/xml; CHARACTERSET=UTF-8
-  \r
-  <${postType}>
-      ${
-        postType === "chg"
-          ? `<requestor id="${postObject.requestor["@id"]}"/>
-        <category id="${postObject.category["@id"]}"/>`
-          : `<customer id="${postObject.customer["@id"]}"/>
-        <category REL_ATTR="${postObject.category["@REL_ATTR"]}"/>`
-      }
-        <z_cst_phone>${postObject.z_cst_phone}</z_cst_phone>
-        <priority id="${postObject.priority["@id"]}"/>
-        <Urgency id="${postObject.Urgency["@id"]}"/>
-        <z_ipaddress>${postObject.z_ipaddress}</z_ipaddress>
-        <z_username>${postObject.z_username}</z_username>
-        <z_computer_name>${postObject.z_computer_name}</z_computer_name>
-        <z_current_loc>${postObject.z_current_loc}</z_current_loc>
-        <z_cst_red_phone>${postObject.z_cst_red_phone}</z_cst_red_phone>
-        <z_network id="${postObject.z_network["@id"]}"/>
-        <z_impact_service id="${postObject.z_impact_service["@id"]}"/>
-        <description>${postObject.description}</description>
-        <z_source id="${postObject.z_source["@id"]}"/>
-        <impact id="${postObject.impact["@id"]}"/>
-  </${postType}>
-  \r
-  --*****MessageBoundary*****\r
-  Content-Disposition: form-data; name="${file.name}"; filename="${file.name}"
-  Content-Type: application/octet-stream
-  Content-Transfer-Encoding: base64
-  \r
-  ${file.base64}
-  \r
-  --*****MessageBoundary*****--\r`;
-};
 
 export { LehavaRouter };
