7bc1953b112a307f72d92e1effc05cba1deefccc
CarmelTsadok <carmelcarmel88888@gmail.com>
2020-11-29 17:02:15 +0200

    fix size limit

--- tommy-client/src/environments/config.dev.ts
+++ tommy-client/src/environments/config.dev.ts
@@ -34,6 +34,6 @@ export const config = {
         return taskBody;
     },
     GET_CATEGORY_KNOWLEDGE_ARTICLE: (categoryId: string) => `/api/caisd-rest/pcat?WC=id%3D${categoryId}`,
-    fileSizeLimit: 70000
+    fileSizeLimit: 50000000
 
 };
