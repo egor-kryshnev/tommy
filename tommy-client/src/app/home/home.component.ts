a808675b8ce84562beebf38e0596973948e5e59d
may kremer <maykremer@gmail.com>
2021-01-04 18:51:22 +0200

    add: lehava user pop up

--- tommy-client/src/app/home/home.component.ts
+++ tommy-client/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { LehavaUserComponent } from '../lehava-user/lehava-user.component';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +11,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   static chatDisplayFlag: Boolean;
-  constructor(private router: Router, private route: ActivatedRoute, public authService: AuthService) { }
+  constructor(private router: Router, private route: ActivatedRoute, public authService: AuthService,public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.openDialog()
   }
 
   public chatTag() {
       document.getElementById("cloack").className = HomeComponent.chatDisplayFlag ? "hidden" : "visible";
       HomeComponent.chatDisplayFlag = !HomeComponent.chatDisplayFlag;
+  }
+
+  openDialog(){
+    this.dialog.open(LehavaUserComponent, {
+      height: '400px',
+      width: '600px',
+      disableClose: true 
+    });
   }
 
 }
